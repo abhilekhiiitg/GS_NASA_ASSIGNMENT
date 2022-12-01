f5e1e395ef4cc6c9e324ed8b0d2d8071e8cb996b
abhilekhiiitg <abhilekhiiitg@gmail.com>
2022-12-01 14:35:33 +0530

    Network Layer implemented

---
+++ app/src/main/java/com/gs/nasa/pod/data/model/ApiError.kt
@@ -0,0 +1,6 @@
+package com.gs.nasa.pod.data.model
+
+data class ApiError(
+    val code: Int,
+    val msg: String? = null
+)
---
+++ app/src/main/java/com/gs/nasa/pod/data/network/PicturesApiService.kt
@@ -0,0 +1,14 @@
+package com.gs.nasa.pod.data.network
+
+
+import com.gs.nasa.pod.data.model.Data
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PicturesApiService {
+
+    @GET("apod?api_key=rCu2dOglgtb1RcjzeutQbeDo4FRbwL8oyuBhJr6h")
+    suspend fun getPictureOfTheDay(@Query("date") date: String): Response<Data>
+}
+
